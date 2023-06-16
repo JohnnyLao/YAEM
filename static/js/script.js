9368bf759804be1d125a384395dd89235d06a390
johnnylao <Johnnylao@mail.ru>
2023-06-16 11:37:16 +0600

    редакция partner.html + счётчик в base

---
+++ static/js/script.js
@@ -0,0 +1,50 @@
+// INDIKATOR
+var time = 2;
+$('#counter').each(function(){
+  $('div').each(function(){
+    var
+    i = 1,
+    num = $(this).data('num'),
+    step = 1200 * time / num,
+    that = $(this),
+    int = setInterval(function(){
+      if (i <= num) {
+        that.html(i);
+      }
+      else {
+        clearInterval(int);
+      }
+      i++;
+    },step);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
