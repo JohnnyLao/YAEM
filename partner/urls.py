76e83a4d92c9ec2badd6842b26622dff44bde593
johnnylao <Johnnylao@mail.ru>
2023-06-20 12:59:19 +0600

    Кнопки для добавления и удаления блюда

--- partner/urls.py
+++ partner/urls.py
@@ -4,5 +4,5 @@ from partner.views import Partner
 app_name = "partner"
 
 urlpatterns = [
-    path('partner', Partner.as_view(), name='partner_page')
+    path('partner/', Partner.as_view(), name='partner_page')
 ]
