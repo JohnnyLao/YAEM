906174fed579f125e94491de63f624386c01523a
Артём Захарченко <artemzaharcenko310@gmail.com>
2023-06-21 19:48:37 +0600

    bug fix

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from cart.views import  CartView, AddToCartView, CartClearView
+from cart.views import CartView, AddToCartView, CartClearView
 
 app_name = 'cart'
 
--- static/js/add_to_cart.js
+++ static/js/add_to_cart.js
@@ -1,9 +1,14 @@
-$(document).ready(function() {
-        $(".add-to-cart-form").submit(function(e) {
+<script>
+    $(document).ready(function() {
+        $("form.add-to-cart-form").submit(function(e) {
             e.preventDefault();
             var form = $(this);
             var url = form.attr('action');
             var formData = new FormData(form[0]);
+
+            // Отключаем кнопку добавления
+            var addButton = form.find('button[type="submit"]');
+            addButton.prop('disabled', true);
 
             $.ajax({
                 type: 'POST',
@@ -12,11 +17,20 @@ $(document).ready(function() {
                 processData: false,
                 contentType: false,
                 success: function(response) {
-                    // handle success response
+                    if (response.success) {
+                        // Обновление информации о корзине
+                        $("#cart-info").text("Количество товаров в корзине: " + response.cart_count);
+                    }
+                    else {
+                        // Обработка ошибки
+                        alert(response.error);
+                    }
                 },
-                error: function(xhr, status, error) {
-                    // handle error response
+                complete: function() {
+                    // Включаем кнопку добавления после завершения AJAX-запроса
+                    addButton.prop('disabled', false);
                 }
             });
         });
     });
+</script>
