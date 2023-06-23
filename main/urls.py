1ef1f94223ad6b1483f8b12a234e1f4bb7fcfd83
Артём Захарченко <artemzaharcenko310@gmail.com>
2023-06-23 13:04:56 +0600

    слайдер есть, надо стили подогнать

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_page
 app_name = "main"
 
 urlpatterns = [
-    path('', cache_page(30)(Main.as_view()), name="main_page"),
-    path('menu/<str:url_name>/', cache_page(30)(Menu.as_view()), name="menu_page"),
+    path('', cache_page(0)(Main.as_view()), name="main_page"),
+    path('menu/<str:url_name>/', cache_page(0)(Menu.as_view()), name="menu_page"),
 
 ]
