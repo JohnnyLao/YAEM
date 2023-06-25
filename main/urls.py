22e5e1b3441f3bae1cdf40e413898100a8b57b85
Egor Loshkarev <Johnnylao@mail.ru>
2023-06-25 20:42:32 +0600

    Counter перенес

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,8 @@ from django.views.decorators.cache import cache_page
 app_name = "main"
 
 urlpatterns = [
-    path('', cache_page(30)(Main.as_view()), name="main_page"),
-    path('menu/<str:url_name>/', cache_page(30)(Menu.as_view()), name="menu_page"),
+    path('', (Main.as_view()), name="main_page"),
+    path('menu/<str:url_name>/', (Menu.as_view()), name="menu_page"),
 
 
 ]
