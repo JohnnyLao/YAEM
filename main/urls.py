0c875830e58809588ff6622b3a2632ffd259c502
Artyom <artemzaharcenko310@gmail.com>
2023-07-06 23:53:58 +0600

    YaEm version 1.1(final)

--- main/urls.py
+++ main/urls.py
@@ -4,13 +4,13 @@ from django.views.decorators.cache import cache_page
 from main.views import Main, Menu
 
 # Кеширование на 5 часов
-cache_duration = 0
+cache_duration = 60 * 60
 app_name = "main"
 
 urlpatterns = [
     path("", cache_page(cache_duration)(Main.as_view()), name="main_page"),
     path(
-        "menu/<str:url_name>/",
+        "<str:url_name>/menu",
         cache_page(cache_duration)(Menu.as_view()),
         name="menu_page",
     ),
