767364cd6f7c103b5b372bb722b96c3c67348e50
Artyom <artemzaharcenko310@gmail.com>
2023-07-06 17:29:06 +0600

    YaEm version 1.0

--- YaYem/settings.py
+++ YaYem/settings.py
@@ -112,7 +112,7 @@ if DEBUG:
         os.path.join(BASE_DIR, "static/"),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR / "static/")
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/"),
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- YaYem/urls.py
+++ YaYem/urls.py
@@ -15,3 +15,4 @@ handler404 = "YaYem.views.page_not_found_view"
 if settings.DEBUG:
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
