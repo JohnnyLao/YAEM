748fb32f0ca20e568496473a7f73707d446d5067
Artyom <artemzaharcenko310@gmail.com>
2023-07-07 00:03:00 +0600

    YaEm version 1.2(final)

--- main/urls.py
+++ main/urls.py
@@ -8,10 +8,10 @@ cache_duration = 60 * 60
 app_name = "main"
 
 urlpatterns = [
-    path("", cache_page(cache_duration)(Main.as_view()), name="main_page"),
     path(
         "<str:url_name>/menu",
         cache_page(cache_duration)(Menu.as_view()),
         name="menu_page",
     ),
+    path("", cache_page(cache_duration)(Main.as_view()), name="main_page"),
 ]
