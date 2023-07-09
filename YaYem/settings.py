0dc6fa088bd0545462d834d931e8a0222d2bf66f
Artyom <artemzaharcenko310@gmail.com>
2023-07-09 19:19:29 +0600

    some fix, admin fix

--- YaYem/settings.py
+++ YaYem/settings.py
@@ -112,7 +112,7 @@ if DEBUG:
         os.path.join(BASE_DIR, "static/"),
     ]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/"),
+    STATIC_ROOT = 'static'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
