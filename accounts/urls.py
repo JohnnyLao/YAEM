5ee3861c9e7dbf50950b38e44b9c66bf30bad06a
Egor Loshkarev <Johnnylao@mail.ru>
2023-10-17 21:11:34 +0600

    register.html + login.html + dashboard.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+from accounts.views import Register, Login, Dashboard
 
-app_name = 'accounts'
+cache_duration = 60 * 60 * 5
+app_name = "accounts"
 
 urlpatterns = [
-
+    path(
+        "login",
+        cache_page(cache_duration)(Login.as_view()),
+        name="login",
+    ),
+    path(
+        "register",
+        cache_page(cache_duration)(Register.as_view()),
+        name="register",
+    ),
+    path(
+        "dashboard",
+        cache_page(cache_duration)(Dashboard.as_view()),
+        name="dashboard",
+    ),
 ]
