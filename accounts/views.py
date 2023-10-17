3b57becaf539f1b9ea0b5b73a8124f4de2d8b1d7
Egor Loshkarev <Johnnylao@mail.ru>
2023-10-17 21:11:34 +0600

    register.html + login.html + dashboard.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class Register(TemplateView):
+    template_name = "accounts/register.html"
+
+
+class Login(TemplateView):
+    template_name = "accounts/login.html"
+
+
+class Dashboard(TemplateView):
+    template_name = "accounts/dashboard.html"
