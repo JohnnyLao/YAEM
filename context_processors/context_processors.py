2f73740e1772f19ef0ecc1bd90fbfd6beea61c59
johnnylao <Johnnylao@mail.ru>
2023-10-23 17:37:10 +0600

    Оптимизация SEO + hotfix вёрстки. Замена статичных предложений

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,17 +1,17 @@
 from main.models import Client, Dish
+from random import randint
 
 
 def data_counter_site(request):
     total_dishes = Dish.objects.count()
     total_clients = Client.objects.count()
-    total_orders = total_dishes * 8
+    total_orders = total_clients * randint(50, 100)
 
     return {
         "total_dishes": total_dishes,
         "total_clients": total_clients,
         "total_orders": total_orders,
     }
-
 
 # def cart_items_count(request):
 #     cart = request.session.get('cart', {})
