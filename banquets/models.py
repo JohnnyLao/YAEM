d8a7b9fce432b9ec769a3f56f945c31fb9cde38e
Artyom <artemzaharcenko310@gmail.com>
2023-10-31 13:56:39 +0600

    Фильтрация банкетных залов

--- banquets/models.py
+++ banquets/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+from main.models import City
+
+CAPASITY_CHOICES = [
+    (500, '500'),
+    (1000, '1000'),
+    (2000, '2000'),
+]
+PRICE_CHOICES = [
+    (5000, '5000'),
+    (8000, '8000'),
+    (10000, '10000'),
+]
+
 
 def banquet_logo_upload_to(instance, filename):
     return f"{instance.name.replace(' ', '_').capitalize()}/logo/{filename}"
@@ -9,8 +22,9 @@ class Banquet(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Название банкетного зала')
     description = models.CharField(max_length=256, blank=True, null=True, verbose_name='Описание')
     logo = models.ImageField(verbose_name='Лого', upload_to=banquet_logo_upload_to, blank=True, null=True)
-    capacity = models.PositiveIntegerField(verbose_name='Вместимость')
-    price = models.PositiveIntegerField(verbose_name='Цена')
+    capacity = models.PositiveIntegerField(verbose_name='Вместимость', choices=CAPASITY_CHOICES)
+    price = models.PositiveIntegerField(verbose_name='Цена за человека', choices=PRICE_CHOICES)
+    city = models.ForeignKey(City, models.CASCADE, verbose_name="Город", blank=True, null=True)
     working_time = models.CharField(max_length=20, verbose_name="Рабочее время", blank=True, null=True)
     address = models.CharField(max_length=50, verbose_name="Адрес", blank=True, null=True, unique=True)
     phone = models.CharField(max_length=30, verbose_name="Телефон", blank=True, null=True, unique=True)
--- banquets/views.py
+++ banquets/views.py
@@ -1,14 +1,34 @@
+from django.shortcuts import render, get_object_or_404
+from django.views import View
+
 from banquets.models import Banquet
 from django.views.generic import TemplateView
 
+from main.models import City
 
-class BanquetList(TemplateView):
+
+class BanquetList(View):
     template_name = "banquets/banquet_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['banquets'] = Banquet.objects.all()
-        return context
+    def get(self, request, *args, **kwargs):
+        banquets = Banquet.objects.all()
+        context = {'banquets': banquets}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        banquets = Banquet.objects.all()
+        capacity = request.POST.get('capacity')
+        price = request.POST.get('price_for_person')
+        city = request.POST.get('city')
+        if capacity:
+            banquets = banquets.filter(capacity=capacity)
+        if price:
+            banquets = banquets.filter(price=price)
+        if city:
+            banquets = banquets.filter(city__name=city)
+        context['banquets'] = banquets
+        return render(request, self.template_name, context)
 
 
 class BanquetPage(TemplateView):
@@ -20,4 +40,3 @@ class BanquetPage(TemplateView):
         banquet = Banquet.objects.get(url_name=url_name)
         context['banquets'] = banquet
         return context
-
