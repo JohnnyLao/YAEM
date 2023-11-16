de46b2fb27ac5960fa12eec1688d9aa1db9d36d1
Artyom <artemzaharcenko310@gmail.com>
2023-11-16 15:03:19 +0600

    Переработка БД и вьюшек 2

--- banquets/views.py
+++ banquets/views.py
@@ -60,7 +60,7 @@ class BanquetPage(TemplateView):
         context = super().get_context_data(**kwargs)
         url_name = self.kwargs['url_name']
         banquet_card = BanquetCard.objects.get(url_name=url_name)
-        banquets = banquet_card.banquet_set.all()
-        context['banquet_card'] = banquet_card
-        context['banquets'] = banquets
+        subhall = banquet_card.banquet_set.all()
+        context['banquet'] = banquet_card
+        context['subhall'] = subhall
         return context
