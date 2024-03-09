de609b82d190edb4453e7d4b3859821fefa53bc0
Egor Loshkarev <Johnnylao@mail.ru>
2024-03-09 20:39:25 +0500

    Проверка на доставку

--- cart/views/cart_page.py
+++ cart/views/cart_page.py
@@ -1,4 +1,4 @@
-
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 
@@ -10,12 +10,12 @@ class CartPage(TemplateView):
 
     def get(self, request, establishment_url_name=None, *args, **kwargs):
         service_percent = Client.objects.get(url_name=establishment_url_name)
-
         return render(
             request=request,
             template_name=self.template_name,
             context={
                 'establishment_url': establishment_url_name,
                 'service_percent': service_percent.service,
+                'delivery': service_percent.delivery,
             },
         )
