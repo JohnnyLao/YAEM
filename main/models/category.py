906df0be89cffe32e4c2402383504d7e21dcb9e2
Egor Loshkarev <Johnnylao@mail.ru>
2024-03-26 15:53:44 +0500

    Для залива перед Наурызом

--- main/models/category.py
+++ main/models/category.py
@@ -4,6 +4,16 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30, verbose_name="Категория")
     z_index = models.IntegerField(verbose_name="Порядковый №", blank=True, null=True)
+    client = models.ForeignKey(
+        "main.Client", verbose_name="Клиент", on_delete=models.SET_NULL, null=True
+    )
+    d_name = models.CharField(
+        max_length=50,
+        verbose_name="Имя в базе",
+        help_text="Кухня_Алаверди",
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Категория"
