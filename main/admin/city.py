80c9424441f32730ab921bc4671faef92e7a49c0
johnnylao <Johnnylao@mail.ru>
2024-04-26 12:03:56 +0500

    #

--- main/admin/city.py
+++ main/admin/city.py
@@ -5,6 +5,6 @@ from main.models import City
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
-    list_display = ['id', "name", "slug", 'z_index',]
+    list_display = ["name", "slug", 'z_index',]
     prepopulated_fields = {"slug": ("name",)}
     list_editable = ['z_index',]
